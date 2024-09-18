a1ba0c0019f4bac3e594ae9f1c3cb831df3091aa
Vlada Vasilevna <vladusik-home@mail.ru>
2024-09-18 19:55:52 +0300

    function 2

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+from typing import Dict, List
+
+
 def filter_by_state(transactions, state='EXECUTED'):
     """ Фильтрует список словарей по значению ключа state """
     return [transaction for transaction in transactions if transaction.get('state') == state]
@@ -13,3 +16,20 @@ transactions = [
 
 executed_transactions = filter_by_state(transactions)
 canceled_transactions = filter_by_state(transactions, state='CANCELED')
+
+
+def sort_by_date(transactions: List[Dict], descending: bool = True) -> List[Dict]:
+    """ Сортирует список словарей по дате """
+    return sorted(transactions, key=lambda x: x['date'], reverse=descending)
+
+
+transactions = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+
+sorted_transactions_desc = sort_by_date(transactions)
+sorted_transactions_asc = sort_by_date(transactions, descending=False)
