8e36bd947bfdbe49ea63e476ca48ae8ad8c0efce
Vlada <admin@admins-MacBook-Air.local>
2024-10-12 20:56:45 +0300

    Выполнено

--- src/data_loader.py
+++ src/data_loader.py
@@ -2,24 +2,18 @@ from typing import Any, Dict, List
 
 import pandas as pd
 
-from .logger import logger  # Импортируем логгер
-
 
 def load_transactions_from_csv(file_path: str) -> List[Dict[str, Any]]:
     """Загружает данные о финансовых транзакциях из CSV-файла."""
     try:
         df = pd.read_csv(file_path)
         transactions = df.to_dict(orient="records")
-        logger.info(f"Успешно загружены транзакции из {file_path}.")
         return transactions
     except FileNotFoundError:
-        logger.error(f"Файл не найден: {file_path}.")
         return []
     except pd.errors.EmptyDataError:
-        logger.error(f"Файл пуст: {file_path}.")
         return []
     except pd.errors.ParserError:
-        logger.error(f"Ошибка парсинга CSV в файле: {file_path}.")
         return []
 
 
@@ -28,11 +22,8 @@ def load_transactions_from_excel(file_path: str) -> List[Dict[str, Any]]:
     try:
         df = pd.read_excel(file_path)
         transactions = df.to_dict(orient="records")
-        logger.info(f"Успешно загружены транзакции из {file_path}.")
         return transactions
     except FileNotFoundError:
-        logger.error(f"Файл не найден: {file_path}.")
         return []
     except ValueError:
-        logger.error(f"Ошибка чтения Excel файла: {file_path}.")
         return []
